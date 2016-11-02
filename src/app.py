d19817f7087cf7218fd99d63fa844fa3ae04cfa9
Jane Hillyard <janehillyard@Janes-MacBook-Pro.local>
2016-11-01 17:02:03 -0700

    JaneH added registration connection modules

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 import clean_and_predict as cap
-#import loaddata as ld
+from flask import Flask, request, render_template
+import json
+import requests
+import socket
+import time
+from datetime import datetime
+import loaddata as ld
 app = Flask(__name__)
+PORT = 5353
+REGISTER_URL = "http://10.5.3.92:5000/register"
+DATA = []
+TIMESTAMP = []
+
 
 # home page
 @app.route('/', methods=['GET'])
@@ -17,7 +28,19 @@ def score():
     prediction = model.transform_predict(clean_json)
     ld.load_pred(prediction, obj_id, db)
 
+def register_for_ping(ip, port):
+    registration_data = {'ip': ip, 'port': port}
+    requests.post(REGISTER_URL, data=registration_data)
+
+
 if __name__ == '__main__':
+
+    # Register for pinging service
+    ip_address = socket.gethostbyname(socket.gethostname())
+    print "attempting to register %s:%d" % (ip_address, PORT)
+    register_for_ping(ip_address, str(PORT))
+
+
     db = ld.connect_to_db()
     cleaner = cap.DataCleaner()
     model = cap.MyModel()
