d546da3d414c9c9f84e823d05ca35246d77bf386
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2016-11-02 13:56:52 -0700

    fixed up application and added model

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-import clean_and_predict as cap
+import mymodel as mm
 import json
 import requests
 import socket
 import time
 from datetime import datetime
 import loaddata as ld
+import cleaning_script as cs
 app = Flask(__name__)
 PORT = 5353
 REGISTER_URL = "http://10.5.3.92:5000/register"
@@ -22,14 +23,14 @@ def index():
 def score():
     raw_data = request.json
     ld.load_raw(raw_data, db)
-    clean_json = cleaner.clean_data(raw_data)
+    clean_json = cs.clean(raw_data)
     obj_id = ld.load_clean(clean_json, db)
     prediction = model.transform_predict(clean_json)
     ld.load_pred(prediction, obj_id, db)
 
 @app.route('/load', methods=['GET'])
 def load():
-    data = ld.dump_collection(db, 'raw_t')
+    data = ld.dump_collection(db, 'pred_t')
     return data
 
 def register_for_ping(ip, port):
@@ -48,5 +49,6 @@ if __name__ == '__main__':
 
     db = ld.connect_to_db()
     cleaner = cap.DataCleaner()
-    model = cap.MyModel()
+    model = mm.MyModel()
+    model.load_model('finalized_model.pkl')
     app.run(host='0.0.0.0', port=8080, debug=True)
